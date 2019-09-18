20c70d2115022518fa329f36ad97c4485ec4b83d
auraPasmin <1288@holbertonschool.com>
2019-09-18 21:15:27 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+ * main - Print string to standard error
+ *
+ * Return: Always 1 (Error)
+ */
+#include <unistd.h>
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, \
+			2015-10-19\n", 59);
+	return (1);
+}
