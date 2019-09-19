6efc91dba1f74f3b633866ed43d1b152604f91ed
auraPasmin <1288@holbertonschool.com>
2019-09-19 20:06:33 +0000

    02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar('c');
+		c = c + 1;
+	}
+	return (0);
+}
