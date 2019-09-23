a0da6e8f0f4f493b755f64765e1d92c205628072
auraPasmin <1288@holbertonschool.com>
2019-09-23 22:11:45 +0000

    0x02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+ 
+int print_alphabet(void)
+{
+	char i;
+
+	for (i='a'; i <= 'z'; i++)
+	{	
+		_putchar(i);
+	}
+
+	_putchar(i);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+im am readme.md
