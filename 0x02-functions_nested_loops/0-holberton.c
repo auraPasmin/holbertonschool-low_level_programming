db8e50c408792054eef7c45f4c38ce609530a4ff
auraPasmin <1288@holbertonschool.com>
2019-09-24 13:53:01 +0000

    000000pls

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,26 +1,24 @@
 #include "holberton.h"
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	
-	int i;
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar('H');
-		_putchar('o');
-		_putchar('l');
-		_putchar('b');
-		_putchar('e');
-		_putchar('r');
-		_putchar('t');
-		_putchar('o');
-		_putchar('n');
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
