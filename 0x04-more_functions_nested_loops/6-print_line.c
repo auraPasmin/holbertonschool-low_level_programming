192516592359b172e6c2515dd6a58fe782b680ca
auraPasmin <1288@holbertonschool.com>
2019-09-26 19:51:48 +0000

    keep pushing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,23 +3,22 @@
 /**
   * print_line -  prints lines
   * @n: take a int
-  * @c: take a int
   * Return: On succes line.
   */
 void print_line(int n)
 {
 	int c;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n > 0)
 	{
 		for (c = 0; c <= n; c++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
