21b740dac6ca5f786da6d2dfd53f741d9993e9fd
auraPasmin <1288@holbertonschool.com>
2019-09-26 19:32:20 +0000

    keep pushing

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  * print_line -  prints lines
+  * @n: take a character
+  * Return: void
+  */
+void print_line(int n)
+{
+	int i;
+	
+	
+	if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
