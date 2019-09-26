de5de3c061395bb250e3b80f508bdb10cc1fde5b
auraPasmin <1288@holbertonschool.com>
2019-09-26 18:52:15 +0000

    keep pushing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,13 @@ void more_numbers(void)
 	{
 		for (d = 0; d <= 14; d++)
 		{
+
 			if (d > 9)
 			{
 				_putchar(d / 10 + '0');
-			_putchar(d % 10 + '0');
 			}
+			 _putchar(d % 10 + '0');
+			
 		}
 		_putchar('\n');
 	}
