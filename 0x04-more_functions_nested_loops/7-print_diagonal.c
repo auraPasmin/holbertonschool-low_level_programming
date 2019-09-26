350d9353d7b6a42beaebc45221cb3cf39266d541
auraPasmin <1288@holbertonschool.com>
2019-09-26 20:31:11 +0000

    keep pushing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+  * print_diagonal -  prints lines
+  * @n: take a int
+  * Return: On succes line.
+  */
+void print_diagonal(int n)
+{
+	int c, d;
+
+	if (n > 0)
+	{
+		for (c = 1; c <= n; c++)
+		{
+			for (d = 2; d <= c + 1; d++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			
+			_putchar('\n');
+			
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
