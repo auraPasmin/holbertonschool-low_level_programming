026eb4fbe264a02d8b107dc5b9ac7f42ea50c674
auraPasmin <1288@holbertonschool.com>
2019-10-01 14:19:31 +0000

    keep pushing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 
 /**
-  *
-  *
-  *
+  * reset_to_98 - arameter and updates the value it points to to 98
+  * @n: take a pointer
+  * not return
   */
 void reset_to_98(int *n)
 {
+	*n = 98;
 
 }
