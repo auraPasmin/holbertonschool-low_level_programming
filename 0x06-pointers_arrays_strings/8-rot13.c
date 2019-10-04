b46221e9d6e19d64c2c27bc2bb03c2aa3bce0a13
auraPasmin <1288@holbertonschool.com>
2019-10-04 00:49:23 +0000

    keep pushing

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rot13 - change
+ * @k: take a pointer, string.
+ * Return: k encryptedi
+ */
+char *rot13(char *k)
+{
+	int i, j;
+
+	char *c1 = "abcdefghijklmABCDEFGHIJKLM";
+	char *c2 = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+	for (i = 0; k[i] != '\0'; i++)
+	{
+		for (j = 0; c1[j] != '\0'; j++)
+			if (k[i] == c1[j])
+			{
+				k[i] = c2[j];
+			}
+			else if (k[i] == c2[j])
+			{
+				k[i] = c1[j];
+			}
+	}
+
+	return (k);
+}
