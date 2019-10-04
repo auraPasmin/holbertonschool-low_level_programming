8b1a8b04ea743527e54957d5f92efc4cc0b1578f
auraPasmin <1288@holbertonschool.com>
2019-10-04 00:29:20 +0000

    keep pushing

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,12 +9,12 @@
 char *leet(char *k)
 {
 	int i, j;
-	char *c1 = "a,e,o,t,l,A,E,O,T,L";
-	char *c2 = "4,3,0,7,1,4,3,0,7,1";
+	char *c1 = "aeotlAEOTL";
+	char *c2 = "4307143071";
 
 	for (i = 0; k[i] != '\0'; i++)
 	{
-		for (i = 0; c1[i] != '\0'; j++)
+		for (j = 0; c1[j] != '\0'; j++)
 		{
 		if (k[i] == c1[j])
 		{
