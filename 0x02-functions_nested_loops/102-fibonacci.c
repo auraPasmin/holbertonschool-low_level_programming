39427cbf7abd514e8839a1170c237905b99505f9
auraPasmin <1288@holbertonschool.com>
2019-10-05 21:10:40 +0000

    keep

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,21 +10,17 @@ int main(void)
 	int i;
 	long int n0 = 1;
 	long int n1 = 1;
-	long int fibo = 0;
+	long int fibo;
 
 	for (i = 0; i < 50; ++i)
 	{
 		if (i < 49)
-		{
 			printf("%ld", fibo);
-		}
 		else
-		{
-		printf("%ld", fibo);
+			printf("%ld", fibo);
 		fibo = n0;
 		n0 = n0 + n1;
 		n1 = fibo;
-		}
 	}
 	putchar ('\n');
 	return (0);
