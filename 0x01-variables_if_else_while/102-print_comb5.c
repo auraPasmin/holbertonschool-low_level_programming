980a651858fbfaf74253378a3036ef8c12a7002b
auraPasmin <1288@holbertonschool.com>
2019-10-05 16:04:26 +0000

    keep

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,30 +5,29 @@
   *
   * Return: Always 0 (Success)
   */
-int  main (void)
+
+int main(void)
 {
 	int i, k;
-	
-	for (k =0; k < 99; k++)
+
+	for (k = 0; k < 99; k++)
 	{
-		for (i = 0; i < 100; i++)
+		for (i = k++; i < 100; i++)
 		{
 			putchar(k / 10 + '0');
 			putchar(k % 10 + '0');
 			putchar(' ');
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
-			if(k < 98 || i < 99)
+			if (k < 98 || i < 99)
 			{
 			putchar(',');
 			putchar(' ');
 			}
-
-			
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
 
 
