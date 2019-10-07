58c9a19799f52eab3982a9746fd65e380cf18790
auraPasmin <1288@holbertonschool.com>
2019-10-07 19:37:21 +0000

    keep pushing

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strchr -  copies memory area.
+ * @s: this a pointer
+ * @c: take a char
+ * Return: c or Null
+ */
+char *_strchr(char *s, char c)
+{
+
+	for (;; s++)
+	{
+		if (*s == c)
+			return ((char *) s);
+	}
+	return (NULL);
+}
