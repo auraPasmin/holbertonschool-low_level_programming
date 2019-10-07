e8c367679a4c03d9af27d38c974cf8bd0e92009b
auraPasmin <1288@holbertonschool.com>
2019-10-07 21:15:15 +0000

    keep pushing

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,8 +17,15 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j] != '\0' ; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				count++;
+				break;
+			}
 		}
+			if (s[i] != accept[j])
+			{
+				break;
+			}
 	}
-	return (count - 2);
+	return (count);
 }
