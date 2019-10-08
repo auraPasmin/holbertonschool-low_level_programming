245f07fb3f71747864f2d6bade0c2ebb5113d4ac
auraPasmin <1288@holbertonschool.com>
2019-10-08 01:05:33 +0000

    keep pushing

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -  diaGONALS	.
+ * @a: this a pointer
+ * size: int
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int d1 = 0, d2 = 0, i, j;
+
+	for  (i = 0; i < (size * size); i = i + size + 1)
+	{
+		d1 = d1 + a[i];
+	}
+	for (j = size - 1; j < (size * size) - 1; j = j + size - 1)
+	{
+		d2 = d1 + a[j];
+	}
+	printf("%d, %d\n", d1, d2);
+}
+
