fcd7dc1e6a8623d0f92c45ae8e64133ecfec41d8
auraPasmin <1288@holbertonschool.com>
2019-10-08 00:17:09 +0000

    keep pushing

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string -  that sets the value of a pointer to a char
+ * @s: this a doubler pointer
+ * @to: take a pointer
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
