ab7bd86dbd42ce299800e08aa6ef97f456925d3f
auraPasmin <1288@holbertonschool.com>
2019-10-10 13:21:03 +0000

    keep pushing

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,38 +1,39 @@
 #include "holberton.h"
-/**
- * is_prime_number - auxiliar
- * @n:
- * @c:
- * Return: int
- */
- 
-int is_prime(int n, int c);
 
 /**
- * is_prime_number - is prime o no
- * @n:
- * Return: 1 o 0
+ * is_prime_number - auxiliar
+ * @n: take a int
+ * @c: take a int
+ * Return: int
  */
+int is_prime(int n, int c);
+
+ /**
+  * is_prime_number - is prime o no
+  * @n:take a in
+  * @c: take a int
+  * Return: 1 o 0
+  */
 int is_prime_number(int n)
 {
 	return (is_prime(n, 2));
 }
 
 /**
-  * is_prime - funcion 
-  * @n
-  * @c
-  *Return: 
-  */
+ * is_prime - funcion
+ * @n: take a int
+ * @c: take a int
+ * Return: 0 or 1
+ */
 int is_prime(int n, int c)
 {
-	if (n % c == 0 || n < 2) 
+	if (n % c == 0 || n < 2)
 	{
 		return (0);
 	}
-	else if (c > n/2)
+	else if (c > n / 2)
 	{
 		return (1);
 	}
-	return is_prime(n, c + 1);
+	return (is_prime(n, c + 1));
 }
