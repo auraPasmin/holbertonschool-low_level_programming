e27681a430a4d68e333f60f9d7e28874a4ea98f5
auraPasmin <1288@holbertonschool.com>
2019-10-11 19:22:58 +0000

    keep pushing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *  main - this a firs main
+  * @argc: take a int
+  * @argv: take a pointer
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		i++;
+		printf("%s\n", argv[i]);
+		return (0);
+	}
+	return (0);
+}
