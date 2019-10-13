50ea19df7d8e6cd18bdca37488dff06e889eff08
auraPasmin <1288@holbertonschool.com>
2019-10-13 21:33:21 +0000

    keep pushing

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,8 @@
   * @argv: take a pointer
   * Return: 0
   */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argv;
 	printf("%d \n", argc - 1);
 	return (0);
 }
