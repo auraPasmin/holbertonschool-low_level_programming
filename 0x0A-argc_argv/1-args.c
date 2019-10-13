213b6274705b3de65e47360f877402ef3e93fbba
auraPasmin <1288@holbertonschool.com>
2019-10-13 21:31:16 +0000

    keep pushing

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  *  main - this a firs main
+  * main - this a firs main
   * @argc: take a int
   * @argv: take a pointer
   * Return: 0
@@ -9,9 +9,6 @@
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	
-
-	if (argc > 0)
-		printf("%d \n", argc - 1);
+	printf("%d \n", argc - 1);
 	return (0);
 }
