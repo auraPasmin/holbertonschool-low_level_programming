18e5a82c121b798fd4c51e98b2f0060973dd2fd5
auraPasmin <1288@holbertonschool.com>
2019-10-15 19:45:05 +0000

    keep pusihng

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - create a array
+ * @str: take a pointer
+ * Return: array or NULL
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *new_str;
+	int size = 1;
+
+	new_str = malloc(size * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_str = malloc(size * sizeof(char));
+		while (str[i] != '\0')
+		{
+			size++;
+			i++;
+		}
+		for (i = 0; i <= (int)size; i++)
+		{
+			*(new_str + i) = *(str + i);
+		}
+		return (new_str);
+	}
+	return (new_str);
+}
