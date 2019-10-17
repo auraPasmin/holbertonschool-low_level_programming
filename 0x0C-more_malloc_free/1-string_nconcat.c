c409dbba164a4bf9b2d9a6cceeadfbb2591280a2
auraPasmin <1288@holbertonschool.com>
2019-10-17 18:01:15 +0000

    keep pushing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = j;
 	}
-	p = malloc((sizeof(char) * (i + n + 1)));
+	p = malloc((sizeof(char) * (i + n) + 1));
 	if (p != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
@@ -44,7 +44,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p[i] = '\0';
 	return (p);
 	}
-	else 
+	else
 	{
 		return (NULL);
 	}
