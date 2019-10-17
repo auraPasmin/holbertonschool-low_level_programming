d4dffe7922dec15793e96589713c35fc358aef48
auraPasmin <1288@holbertonschool.com>
2019-10-17 20:58:17 +0000

    keep pushing

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,28 +6,28 @@
  * array_range - creates an array of integers.
  * @min: take a pointer
  * @max: take a pointer
- * Return: A pointer or if fails return 98
+ * Return: prt or null
  *
  */
 int *array_range(int min, int max)
 {
-	char *prt;
-	int i, size;
+	int i = 0;
+	int *prt;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	size = max - min + 1;
-	prt = malloc(size * sizeof(*p));
-	if (p != NULL)
+	prt = malloc((max - min ) + 1 * sizeof(int));
+	if (prt == NULL)
 	{
-	for (i = 0; i < size; i++)
+		return (NULL);
+	}
+	while (i <= max - min)
 	{
 		prt[i] = min;
 		min++;
+		i++;
 	}
 	return (prt);
-	}
-	return (NULL);
 }
