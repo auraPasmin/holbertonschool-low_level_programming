cf2688bca1d3f0e9e336117ee1ede7325817b166
auraPasmin <1288@holbertonschool.com>
2019-10-21 23:47:14 +0000

    keep pushing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
   * @owner: take a pointer
   */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *, float, char *);
 void print_dog(struct dog *d);
 #endif
