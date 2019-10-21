13e8fa07ab71577a477341b61543db991859c70b
auraPasmin <1288@holbertonschool.com>
2019-10-21 23:00:29 +0000

    keep pushing

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+  * struct dog - the is strcut
+  * @name: take a pointer
+  * @age: take a float
+  * @owner: take a pointer
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *, float, char *);
+#endif
