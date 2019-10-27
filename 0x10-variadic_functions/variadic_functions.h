46b55062290d11dd434a1f2806adc62350826c4a
auraPasmin <1288@holbertonschool.com>
2019-10-27 10:46:58 -0500

    removing string library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>:
 #include <stdarg.h>
-#include <strings.h>
+
 typedef struct arg
 {
 	char *format;
