babfc8a2902ed286b704c68878deea835861fd79
auraPasmin <1288@holbertonschool.com>
2019-11-07 22:55:32 -0500

    kp

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * get_bit - get a bit in a position
+ *@n: is a unsigned long int
+ *@index: is a unsigned int
+ * Return: On succes.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+	{
+		return  (-1);
+	}
+	return (n >> index & 1);
+}
