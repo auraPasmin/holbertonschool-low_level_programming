d3443287022f3812aa50a35c54c0cdfa5c5fdfd1
auraPasmin <1288@holbertonschool.com>
2019-11-12 18:50:46 -0500

    kp

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * read_textfile -  Function that reads a text file and prints it to the POSIX
+ * @filename: is a pointer
+ * @letters: is a number o letters
+ * Return:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fileOP, fileRead, fileWrite;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(letters) * letters);
+
+	if (buff == NULL)
+		return (0);
+
+	fileOP = open(filename, O_RDONLY);
+
+	if (fileOP == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	fileRead = read(fileOP, buff, letters);
+	if (fileRead == -1)
+	{
+		free(buff);
+		close(fileOP);
+		return (0);
+	}
+	fileWrite = write(STDOUT_FILENO, buff, fileRead);
+	if (fileWrite == -1)
+	{
+		free(buff);
+		close(fileRead);
+		return (0);
+	}
+	free(buff);
+	close (fileRead);
+	return (fileWrite);
+}
