640528588f19bd333a3ba49b3509b2afa0fd1fd2
auraPasmin <1288@holbertonschool.com>
2019-12-15 17:02:50 +0000

    kp

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Function that prints all the elements of  lisi
+ *@h: A pointer
+ *Return: The nmber of nodes in the doubly linked list of type dlistint_t
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t c = 0;
+	const dlistint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		c++;
+		temp = temp->next;
+	}
+	return (c);
+}
