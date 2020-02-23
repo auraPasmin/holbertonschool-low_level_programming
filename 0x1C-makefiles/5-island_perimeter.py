53a955c11994031c0ab5f4b26fb6685140f8eddf
auraPasmin <1288@holbertonschool.com>
2020-02-23 14:21:16 -0500

    fix 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""island_perimeter"""
+
+
+def island_perimeter(grid):
+	par = 0
+	numrows = len(grid)
+	numcols = len(grid[0])
+	y = -1
+	for row in grid:
+	    y += 1
+	    x = -1
+	    for element in row:
+	        x += 1
+	        if element == 1:
+	            if x == 0 or grid[y][x - 1] != 1:
+	                par += 1
+	            if x == numcols - 1 or grid[y][x + 1] != 1:
+	                par += 1
+	            if y == 0 or grid[y - 1][x] != 1:
+	                par += 1
+	            if y == numrows - 1 or grid[y + 1][x] != 1:
+	                par += 1
+	return (par)
